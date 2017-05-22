61f96b21b73413f18aad31ae677d7f362e196bf7
Nils <imperssonator@gmail.com>
2017-05-22 13:32:05 -0400

    small bug fix with EPMatch
    also added objective_overlap and run_train

--- Functions/Training/Run_Train.m
+++ Functions/Training/Run_Train.m
@@ -1,50 +1,56 @@
-im_path = '/Users/Imperssonator/Google Drive/Kaylie/Fiber Growth master/For FiberApp/S2U0T20B2D0126.001.tif';
-fa_path = '/Users/Imperssonator/Google Drive/Kaylie/Fiber Growth master/For FiberApp/S2U0T20B2D0126.001a.mat';
+if ispc
+    im_path = 'C:\Users\npersson3\Google Drive\Kaylie\Fiber Growth master\For FiberApp\S2U0T9B20118.002.tif';
+    fa_path = 'C:\Users\npersson3\Google Drive\Kaylie\Fiber Growth master\For FiberApp\S2U0T9B20118.002.mat';
+else
+    im_path = '/Users/Imperssonator/Google Drive/Kaylie/Fiber Growth master/For FiberApp/S2U0T20B2D0126.001.tif';
+    fa_path = '/Users/Imperssonator/Google Drive/Kaylie/Fiber Growth master/For FiberApp/S2U0T20B2D0126.001a.mat';
+end
+
 nmWid = 5000;
 
 x0 = [...
-10;
-30;
-3;
-30;
-1500;
-80;
-80;
-0.1;
-3];
+    10;
+    30;
+    3;
+    30;
+    1500;
+    80;
+    80;
+    0.1;
+    3];
 
 lb = [...
-1;
-1;
-0.5;
-1;
-100;
-0;
-0;
-0;
-2];
+    1;
+    1;
+    0.5;
+    1;
+    100;
+    0;
+    0;
+    0;
+    2];
 
 ub = [...
-100;
-100;
-10;
-100;
-5000;
-300;
-500;
-0.2;
-10];
+    100;
+    100;
+    10;
+    100;
+    5000;
+    300;
+    500;
+    0.2;
+    10];
 
 del = [...
-10;
-10;
-2;
-10;
-500;
-10;
-10;
-0.01;
-1];
+    10;
+    10;
+    2;
+    10;
+    500;
+    10;
+    10;
+    0.01;
+    1];
 
 ofun = @(p) objective_overlap(p,im_path,nmWid,fa_path);
 
