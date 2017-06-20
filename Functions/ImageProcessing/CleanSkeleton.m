1f7f2736d345a8801af2599b34336b1d40ef1d3b
Nils <imperssonator@gmail.com>
2017-06-19 20:38:29 -0400

    stopped closing skeleton
    send to Windows

--- Functions/ImageProcessing/CleanSkeleton.m
+++ Functions/ImageProcessing/CleanSkeleton.m
@@ -4,10 +4,10 @@ function ims = CleanSkeleton(ims)
 % until it consists only of unbranched, isolated segments.
 
 % Closing to remove small holes
-skelClose = bwmorph(imclose(ims.skel,strel('disk',1)),'skeleton',Inf);
+% skelClose = bwmorph(imclose(ims.skel,strel('disk',1)),'skeleton',Inf);
 
 % Remove branches up to a certain size
-ims.skelTrim = RemoveBranches(skelClose,ims.settings.maxBranchSize);
+ims.skelTrim = RemoveBranches(ims.skel,ims.settings.maxBranchSize);
 
 % Find branch points, dilate them, remove the dilated points to separate
 % all segments
